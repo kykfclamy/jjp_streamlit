c0518eb50bcff95a3d900a3d7f8defbc88e50b4d
LK <kykfclamy@gmail.com>
2024-02-21 15:52:14 +0800

    Create jjp_sl.py

---
+++ jjp_sl.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 21/02/2024
+
+@author: kming
+"""
+import streamlit as st
+import time
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+st.set_page_config(page_title='JJP - Study',  layout='wide', page_icon=':ambulance:')
+
+#this is the header
+ 
+t1, t2 = st.columns((0.07,1)) 
+
+t2.title("This is a test --- jjp study streamlit")
+### t2.markdown(" **tel:** 01392 451192 **| website:** https://www.swast.nhs.uk **| email:** mailto:data.science@swast.nhs.uk")
+
